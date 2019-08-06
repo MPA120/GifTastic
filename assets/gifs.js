be7ecc6786ecc0763624bce8c3071e435437fdce
MPA <50469825+MPA120@users.noreply.github.com>
2019-08-05 21:13:07 -0400

    added initial JS and links

---
+++ assets/gifs.js
@@ -0,0 +1,10 @@
+var startingAnimals = ["Lion", "Elephant", "Duck", "Pig", "Hippopotamus","Cheetah","Ostrich", "Polar Bear", "Grizzly Bear", "Giraffe","Zebra"];
+
+var numberOfGifs = 11;
+var cutOffRating = "PG";
+
+function renderButton(){
+    for(var i=0; i < startingAnimals.length; i++){
+        var newButton = $("<button>")
+    }
+}
