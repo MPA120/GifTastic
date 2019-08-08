f3f062b6b943c2e0fb6310fd67f75122a622bf55
MPA <50469825+MPA120@users.noreply.github.com>
2019-08-08 18:00:36 -0400

    first submit

--- assets/gifs.js
+++ assets/gifs.js
@@ -1,10 +1,87 @@
-var startingAnimals = ["Lion", "Elephant", "Duck", "Pig", "Hippopotamus","Cheetah","Ostrich", "Polar Bear", "Grizzly Bear", "Giraffe","Zebra"];
+$(document).ready(function(){
 
-var numberOfGifs = 11;
-var cutOffRating = "PG";
+    var topics = ["Duck", "Giraffe", "Lion","Pig", "Dog"];
 
-function renderButton(){
-    for(var i=0; i < startingAnimals.length; i++){
-        var newButton = $("<button>")
+    function displayImg(){
+
+        $("#display-images").empty();
+        var input = $(this).attr("data-name");
+        var limit = 10;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=" + limit + "&api_key=8eUS4EqnDIKdCvBN3ULoSL6LSQDJQxvz";   
+
+        $.ajax({
+            url: queryURL, 
+            method: "GET"
+        }).done(function(response) {
+
+            for(var j = 0; j < limit; j++) {    
+
+                var displayDiv = $("<div>");
+                displayDiv.addClass("holder");
+            
+                var image = $("<img>");
+                image.attr("src", response.data[j].images.original_still.url);
+                image.attr("data-still", response.data[j].images.original_still.url);
+                image.attr("data-animate", response.data[j].images.original.url);
+                image.attr("data-state", "still");
+                image.attr("class", "gif");
+                displayDiv.append(image);
+
+                var rating = response.data[j].rating;
+                console.log(response);
+                var pRating = $("<h3>").text("Rating: " + rating);
+                displayDiv.append(pRating)
+
+                $("#display-images").append(displayDiv);
+            }
+        });
     }
-}
+
+    function renderButtons(){ 
+
+        $("#display-buttons").empty();
+
+        for (var i = 0; i < topics.length; i++){
+
+            var newButton = $("<button>") 
+            newButton.attr("class", "btn btn-default");
+            newButton.attr("id", "input")  
+            newButton.attr("data-name", topics[i]); 
+            newButton.text(topics[i]); 
+            $("#display-buttons").append(newButton); 
+        }
+    }
+
+    function imageChangeState() {          
+
+        var state = $(this).attr("data-state");
+        var animateImage = $(this).attr("data-animate");
+        var stillImage = $(this).attr("data-still");
+
+        if(state == "still") {
+            $(this).attr("src", animateImage);
+            $(this).attr("data-state", "animate");
+        }
+
+        else if(state == "animate") {
+            $(this).attr("src", stillImage);
+            $(this).attr("data-state", "still");
+        }   
+    }
+
+    $("#submitClick").on("click", function(){
+
+        var input = $("#user-input").val().trim();
+        form.reset();
+        topics.push(input);
+                
+        renderButtons();
+
+        return false;
+    })
+
+    renderButtons();
+
+    $(document).on("click", "#input", displayImg);
+    $(document).on("click", ".gif", imageChangeState);
+});
